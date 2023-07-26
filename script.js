2b18a04ae993a2e52edc02eb7c64176f521cdcaf
Yevhen Barkov <mr.e.barkov@gmail.com>
2023-07-26 22:41:46 +0300

    modified check method

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 class SuperMath {
-    input() {
-      const X = parseFloat(prompt("Введіть X:"));
-      const Y = parseFloat(prompt("Введіть Y:"));
-      const znak = prompt("Введіть дію ('+','-','*','/'):");
-  
-      const obj = { X, Y, znak };
-  
-      return obj;
-    }
-  
-    check({ X, Y, znak }) {
-      const userInput = confirm(`Виконати дію ${znak} з атрибутами ${X} і ${Y}?`);
-  
-      if (userInput) {
-        const actions = {
-          '+': (x, y) => x + y,
-          '-': (x, y) => x - y,
-          '*': (x, y) => x * y,
-          '/': (x, y) => x / y
-        };
-  
-        const action = actions[znak];
-        if (action) {
-          return action(X, Y);
-        } else {
-          return 'Помилка';
-        }
+  input() {
+    const X = parseFloat(prompt("Введіть X:"));
+    const Y = parseFloat(prompt("Введіть Y:"));
+    const znak = prompt("Введіть дію ('+','-','*','/'):");
+
+    const obj = { X, Y, znak };
+
+    return obj;
+  }
+
+  check() {
+    const obj = this.input();
+    const { X, Y, znak } = obj;
+
+    const userInput = confirm(`Виконати дію ${znak} з атрибутами ${X} і ${Y}?`);
+
+    if (userInput) {
+      const actions = {
+        "+": (x, y) => x + y,
+        "-": (x, y) => x - y,
+        "*": (x, y) => x * y,
+        "/": (x, y) => x / y,
+      };
+
+      const action = actions[znak];
+      if (action) {
+        const result = action(X, Y);
+        alert(`Результат: ${result}`);
+        return result;
       } else {
-        return 'Операція відмінена користувачем';
+        alert("Помилка");
+        return "Помилка";
       }
+    } else {
+      alert("Операція відмінена користувачем");
+      return "Операція відмінена користувачем";
     }
   }
-  
-    const superMath = new SuperMath();
-    const obj = superMath.input();
-    const result = superMath.check(obj);
-    alert(`Результат: ${result}`);
+}
+
+const superMath = new SuperMath();
+superMath.check();
